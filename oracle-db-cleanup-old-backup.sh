ad88e5a2eda82d462e79f7b0aaabd39712b2223a
Stéphane Gully <gully@abes.fr>
2024-02-23 12:46:08 +0100

    fix: old backup files were not deleted

--- oracle-db-cleanup-old-backup.sh
+++ oracle-db-cleanup-old-backup.sh
@@ -9,9 +9,13 @@ if [ "$FILES_TO_RM" == "" ]; then
     echo "-> No backup files to remove older than $ORACLE_DB_BACKUP_CLEANUP_TIME days, here is the (not to cleanup yet) files:"
     find /backup/ -type f
 else
-    echo "-> Following backup files are going to be deleted (older than $ORACLE_DB_BACKUP_CLEANUP_TIME days):"
+    NB_TO_DELETE=$(find /backup/ -not -newer /tmp/oracle-db-cleanup-ref -type f | wc -l)
+    echo "-> Following backup files are going to be deleted ($NB_TO_DELETE files will be deleted because older than $ORACLE_DB_BACKUP_CLEANUP_TIME days):"
+    # show what will be deleted
     find /backup/ -not -newer /tmp/oracle-db-cleanup-ref -type f
-    find /backup/ -not -newer /tmp/oracle-db-cleanup-ref -type f -exec ls {} \;
+    # really delete it
+    find /backup/ -not -newer /tmp/oracle-db-cleanup-ref -type f -exec rm -f {} \;
+    echo "-> Cleanup backup finished: $NB_TO_DELETE files were deleted"
 fi 
 
 
